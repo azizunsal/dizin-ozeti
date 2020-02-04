976e8b2ff3f981fc261c000ac0b2694bcc218f44
Taha Furkan Unsal <furkantahaunsal@gmail.com>
2020-02-04 22:36:18 +0300

    guncelledim

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <dirent.h>
 
-int main() {
-    DIR *d;
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
 
-    struct dirent *dir;
-    d = opendir("C:\\test");
+int main(void) {
+    struct dirent *de;
+    int txtDosyaSayisi = 0;
 
-    if (d) {
-        while ((dir == readdir(d)) != NULL) {
-            printf("%s\n", dir->d_name);
-        }
-        closedir(d);
+    DIR *dr = opendir("./deneme");
+
+    if (dr == NULL) {
+        printf("dosya dizini bulunamadi...\n");
+        return 0;
     }
+
+    while ((de = readdir(dr)) != NULL) {
+        if (de->d_name[(strlen(de->d_name) - 3)] == 't' && de->d_name[(strlen(de->d_name) - 2)] == 'x' &&
+            de->d_name[(strlen(de->d_name) - 1)] == 't')
+            txtDosyaSayisi++;
+        printf("%s\n", de->d_name);
+    }
+    printf("Toplam %d tane 'txt' dosyasi bulunmaktadir.\n", txtDosyaSayisi);
+    closedir(dr);
     return 0;
 }
